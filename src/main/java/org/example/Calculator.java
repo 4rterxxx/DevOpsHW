5f0830ab0da09db9cf6454f42e499c385798890c
Aleksey <tryaskov.alex@yandex.ru>
2024-06-09 23:09:05 +0300

    tests added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class Calculator {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
